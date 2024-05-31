486708ce8ab663ca94333eb8e0917be8139f5977
rvmu-git <rvmu@rvmu.es>
2024-05-31 11:59:17 +0200

    * 3

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using WebApplication1.Models;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AdventureWorks2016Context>(options =>
